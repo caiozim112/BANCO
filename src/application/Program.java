bebc7e4d1b5734bbce404bab5d118e7ed3240376
Caiozim112 <caioopua@gmail.com>
2021-12-02 13:13:03 -0300

    quarto commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,6 +16,10 @@ public class Program {
 		Department obj2 = new Department(1,"Books");
 		Seller seller = new Seller(21,"bob", "caio@gmail.copm",new Date("10/06/2002"),1000.00, obj2);
 		System.out.println(seller );
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println(seller);
 	}
 
 }
